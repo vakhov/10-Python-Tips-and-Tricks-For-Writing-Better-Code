2971d22b95e79d3da1a194d71a80cef1d5a94e1c
Alexander Vakhov <alexander@vakhov.ru>
2019-03-30 14:58:33 +0500

    GetPass

---
+++ password.py
@@ -0,0 +1,11 @@
+username = input('Username: ')
+password = input('Password: ')
+
+print('Loggin In...')
+
+from getpass import getpass
+
+username = input('Username: ')
+password = getpass('Password: ')
+
+print('Loggin In...')
