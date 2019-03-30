56164d5c79c930aa3cc5be4e1e3ac061d58a7c3d
Alexander Vakhov <alex.vakhov@gmail.com>
2019-03-30 13:44:03 +0500

    Ternary Conditionals

---
+++ one.py
@@ -0,0 +1,16 @@
+# Ternary Conditionals
+condition = True
+
+if condition:
+    x = 1
+else:
+    x = 0
+
+print(x)
+
+# #######################################
+
+
+x = 1 if condition else 0
+
+print(x)
