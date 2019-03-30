aa37158bcd9637adb40455c7782be05a436adff3
Alexander Vakhov <alex.vakhov@gmail.com>
2019-03-30 13:48:19 +0500

    Underscore Placeholders

---
+++ two.py
@@ -0,0 +1,18 @@
+# Underscore Placeholders
+
+num1 = 10000000000
+num2 = 100000000
+
+total = num1 + num2
+
+print(total)
+
+# #######################################
+
+
+num1 = 10_000_000_000
+num2 = 100_000_000
+
+total = num1 + num2
+
+print(f'{total:,}')
